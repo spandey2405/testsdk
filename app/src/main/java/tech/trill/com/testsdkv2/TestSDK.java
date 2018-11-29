f0306c15dba1120d65f840d279844b6726c16de0
sid <saurabh@trillbit.com>
2018-11-29 22:57:18 +0530

    Added Files

---
+++ app/src/main/java/tech/trill/com/testsdkv2/TestSDK.java
@@ -0,0 +1,17 @@
+package tech.trill.com.testsdkv2;
+
+import android.util.Log;
+
+/**
+ * Created by sid on 29/11/18.
+ */
+
+public class TestSDK {
+    public TestSDK() {
+        Log.e("TestSDK", "SDK Testing Started");
+    }
+
+    public void complete() {
+        Log.e("TestSDK", "SDK Testing Completed");
+    }
+}
